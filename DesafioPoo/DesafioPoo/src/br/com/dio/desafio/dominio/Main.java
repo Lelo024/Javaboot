bb25538425cf820f53db467d59db794d6baa02cc
lelo24 <leticia2mm@gmail.com>
2024-07-04 21:31:57 -0300

    Final

--- DesafioPoo/DesafioPoo/src/br/com/dio/desafio/dominio/Main.java
+++ DesafioPoo/DesafioPoo/src/br/com/dio/desafio/dominio/Main.java
@@ -19,8 +19,30 @@ public class Main {
         mentoria.setDescricao("Mentoria aos alunos do curso Java");
         mentoria.setData(LocalDate.now());
         
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        BootCamp bootCamp = new BootCamp();
+        bootCamp.setNome("Bootcamp Java Developer");
+        bootCamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootCamp.getConteudos().add(curso1);
+        bootCamp.getConteudos().add(curso2);
+        bootCamp.getConteudos().add(mentoria);
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootCamp(bootCamp);
+        System.out.println("Conteúdos Incritos Camila:" + devCamila.getConteudosInscritos());
+        devCamila.progredir();
+        System.out.println("Conteúdos Concluídos Camila:" + devCamila.getConteudosConcluidos());
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("João");
+        devJoao.inscreverBootCamp(bootCamp);
+        System.out.println("Conteúdos Incritos João:" + devJoao.getConteudosInscritos());
+        devJoao.progredir();
+        System.out.println("Conteúdos Concluídos João:" + devJoao.getConteudosConcluidos());
+
     }
 }
