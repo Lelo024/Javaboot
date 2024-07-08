245f2fb9ba7712acfe5db07b821a6c45f3445c0f
lelo24 <leticia2mm@gmail.com>
2024-07-08 19:36:20 -0300

    attfinal

---
+++ AplicacaoAPI/SanBoot/SanBoot/src/main/java/me/SanBoot/SanBoot/SanBootApplication.java
@@ -0,0 +1,13 @@
+package me.SanBoot.SanBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanBootApplication.class, args);
+	}
+
+}
---
+++ AplicacaoAPI/SanBoot/SanBoot/src/test/java/me/SanBoot/SanBoot/SanBootApplicationTests.java
@@ -0,0 +1,13 @@
+package me.SanBoot.SanBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
